72ac5357360a70f4adda574218169a39f7c9116f
Gonzalo Juarez <gonzalojuarez@gmail.com>
2025-09-27 13:34:09 +0200

    fix underscores and subscripts relation

--- build-site.el
+++ build-site.el
@@ -20,7 +20,8 @@
 (setq org-html-validation-link nil            ;; Don't show validation link
       org-html-head-include-scripts nil       ;; Use our own scripts
       org-html-head-include-default-style nil ;; Use our own styles
-      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
+      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />"
+      org-export-with-sub-superscripts '{})
 
 ;; Define the publishing project
 (setq org-publish-project-alist
@@ -30,10 +31,10 @@
              :base-directory "./content"
              :publishing-function 'org-html-publish-to-html
              :publishing-directory "./public"
-             :with-author nil           ;; Don't include author name
-             :with-creator t            ;; Include Emacs and Org versions in footer
-             :with-toc t                ;; Include a table of contents
-             :section-numbers nil       ;; Don't include section numbers
+             :with-author nil ;; Don't include author name
+             :with-creator t ;; Include Emacs and Org versions in footer
+             :with-toc t     ;; Include a table of contents
+             :section-numbers nil    ;; Don't include section numbers
              :time-stamp-file nil)))    ;; Don't include time stamp in file
 
 ;; Generate the site output
