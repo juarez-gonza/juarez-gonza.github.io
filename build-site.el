d28fc0b22c8efa54582b3b1f6dcb5d9820990a3a
Gonzalo Juarez <gonzalojuarezte@gmail.com>
2026-09-09 00:59:31 +0200

    change style and center images

--- build-site.el
+++ build-site.el
@@ -22,8 +22,12 @@
 (setq org-html-validation-link nil      ;; Don't show validation link
       org-html-head-include-scripts nil ;; Use our own scripts
       org-html-head-include-default-style nil ;; Use our own styles
-      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />"
-      org-export-with-sub-superscripts '{})
+      org-export-with-sub-superscripts '{}
+      ;; style: simple css
+      ;; org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />"
+      ;; style: gongzhitaao
+      org-html-htmlize-output-type 'css ;; needed for gongzhitaao code highlighting
+      org-html-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"https://gongzhitaao.org/orgcss/org.css\" />")
 
 ;; Define the publishing project
 (setq org-publish-project-alist
